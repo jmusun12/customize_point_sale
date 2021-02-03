4336efd03aa7d09e7b8eff35d1bbe4fa5213ce17
jmusun12 <stdjosemusun@gmail.com>
2021-02-03 17:38:50 -0600

    Override javascript

--- models/pos_order.py
+++ models/pos_order.py
@@ -1,5 +1,6 @@
-from odoo import api, models
+from odoo import api, models, _
 import base64
+import logging
 
 
 class PosOrder(models.Model):
@@ -8,7 +9,7 @@ class PosOrder(models.Model):
     def action_invoice_to_customer(self, name=None, client=None):
         self.ensure_one()
 
-        report = self.env.ref('customize_point_sale.pos_invoice_report_imagen').render_qweb_pdf(self.ids)
+        report = self.env.ref('customize_point_sale.pos_invoice_report_imagen').render_qweb_pdf(self.account_move.id)
         filename = self.name + '.pdf'
         attachment = self.env['ir.attachment'].create({
             'name': filename,
@@ -29,3 +30,52 @@ class PosOrder(models.Model):
             "url": str(base_url) + str(download_url),
             'target': 'new',
         }
+
+    @api.model
+    def action_receipt_to_customer_override(self, name, client, ticket, order_ids=False):
+        logging.warning('Overrid action_receipt_to_customer')
+
+        # FIXME MASTER: make a true multi
+        if not self.env.user.has_group('point_of_sale.group_pos_user'):
+            return False
+        if not client.get('email'):
+            return False
+        orders = self.browse(order_ids) if order_ids else self
+
+        message = _("<p>Dear %s,<br/>Here is your electronic ticket for the %s. </p>") % (client['name'], name)
+
+        filename = 'Receipt-' + name + '.jpg'
+        receipt = self.env['ir.attachment'].create({
+            'name': filename,
+            'type': 'binary',
+            'datas': ticket,
+            'res_model': 'pos.order',
+            'res_id': orders[:1].id,
+            'store_fname': filename,
+            'mimetype': 'image/jpeg',
+        })
+        template_data = {
+            'subject': _('Receipt %s') % name,
+            'body_html': message,
+            'author_id': self.env.user.partner_id.id,
+            'email_from': self.env.company.email or self.env.user.email_formatted,
+            'email_to': client['email'],
+            'attachment_ids': [(4, receipt.id)],
+        }
+
+        if orders.mapped('account_move'):
+            report = self.env.ref('customize_point_sale.report_invoice_imagen_pdv').render_qweb_pdf(orders.ids[0])
+            filename = name + '.pdf'
+            attachment = self.env['ir.attachment'].create({
+                'name': filename,
+                'type': 'binary',
+                'datas': base64.b64encode(report[0]),
+                'store_fname': filename,
+                'res_model': 'pos.order',
+                'res_id': orders[:1].id,
+                'mimetype': 'application/x-pdf'
+            })
+            template_data['attachment_ids'] += [(4, attachment.id)]
+
+        mail = self.env['mail.mail'].create(template_data)
+        mail.send()
--- static/src/js/screen.js
+++ static/src/js/screen.js
@@ -5,6 +5,11 @@ odoo.define('pos_invoice_customize.screens', function (require) {
     var screens = require('point_of_sale.screens');
     var rpc = require('web.rpc');
     var _t = core._t;
+    var gui = require('point_of_sale.gui');
+    var Printer = require('point_of_sale.Printer').Printer;
+    var QWeb = core.qweb;
+
+
 
     screens.ReceiptScreenWidget.include({
         renderElement: function() {
@@ -32,4 +37,122 @@ odoo.define('pos_invoice_customize.screens', function (require) {
             });
         },
     });
+
+
+    var PaymentScreenWidget = screens.PaymentScreenWidget.extend({
+        template:      'PaymentScreenWidget',
+
+        send_receipt_to_customer: function(order_server_ids) {
+            var order = this.pos.get_order();
+            var data = {
+                widget: this,
+                pos: order.pos,
+                order: order,
+                receipt: order.export_for_printing(),
+                orderlines: order.get_orderlines(),
+                paymentlines: order.get_paymentlines(),
+            };
+
+            var receipt = QWeb.render('OrderReceipt', data);
+            var printer = new Printer();
+
+            alert('Widget Override Success');
+
+            return new Promise(function (resolve, reject) {
+                printer.htmlToImg(receipt).then(function(ticket) {
+                    rpc.query({
+                        model: 'pos.order',
+                        method: 'action_receipt_to_customer_override',
+                        args: [order.get_name(), order.get_client(), ticket, order_server_ids],
+                    }).then(function() {
+                        alert('Widget Override Success');
+                        resolve();
+                    }).catch(function () {
+                      order.set_to_email(false);
+                      reject("There is no internet connection, impossible to send the email.");
+                    });
+                });
+            });
+        },
+
+        post_push_order_resolve: function(order, server_ids){
+            var self = this;
+            alert('Widget Override Success');
+            if (order.is_to_email()) {
+                var email_promise = self.send_receipt_to_customer(server_ids);
+                alert('Widget Override Success');
+                return email_promise;
+            }
+            else {
+                return Promise.resolve();
+            }
+        },
+
+        finalize_validation: function() {
+            var self = this;
+            var order = this.pos.get_order();
+
+            if (order.is_paid_with_cash() && this.pos.config.iface_cashdrawer) {
+
+                    this.pos.proxy.printer.open_cashbox();
+            }
+
+            order.initialize_validation_date();
+            order.finalized = true;
+
+            alert('Widget Override Success');
+
+            if (order.is_to_invoice()) {
+                var invoiced = this.pos.push_and_invoice_order(order);
+                this.invoicing = true;
+
+                invoiced.catch(this._handleFailedPushForInvoice.bind(this, order, false));
+
+                invoiced.then(function (server_ids) {
+                    self.invoicing = false;
+                    var post_push_promise = [];
+                    post_push_promise = self.post_push_order_resolve(order, server_ids);
+                    post_push_promise.then(function () {
+                        alert('Widget Override Success');
+                        self.gui.show_screen('receipt');
+                    }).catch(function (error) {
+                        self.gui.show_screen('receipt');
+                        if (error) {
+                            self.gui.show_popup('error',{
+                                'title': "Error: no internet connection",
+                                'body':  error,
+                            });
+                        }
+                    });
+                });
+            } else {
+                var ordered = this.pos.push_order(order);
+                if (order.wait_for_push_order()){
+                    var server_ids = [];
+                    ordered.then(function (ids) {
+                      server_ids = ids;
+                    }).finally(function() {
+                        var post_push_promise = [];
+                        post_push_promise = self.post_push_order_resolve(order, server_ids);
+                        post_push_promise.then(function () {
+                                self.gui.show_screen('receipt');
+                            }).catch(function (error) {
+                              self.gui.show_screen('receipt');
+                              if (error) {
+                                  self.gui.show_popup('error',{
+                                      'title': "Error: no internet connection",
+                                      'body':  error,
+                                  });
+                              }
+                            });
+                      });
+                }
+                else {
+                  self.gui.show_screen('receipt');
+                }
+            }
+        },
+    });
+    gui.define_screen({name:'payment', widget: PaymentScreenWidget});
+
 });
